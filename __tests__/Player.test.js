33fae5e987b220d4256247acb424bb6a10bf4ca9
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-14 18:52:19 -0400

    added mock for potion and refactor player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,15 @@
+// test Potion with mock values
+const Potion = require('../lib/Potion');
+
+// replaces the constructor's implementation with fake data
+jest.mock('../lib/Potion');
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+
+console.log(new Potion());
+
+// test Player
 const Player = require('../lib/Player');
 
 test('creates a player object' , () => {
